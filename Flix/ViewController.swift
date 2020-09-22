9c1e75e949862711ac37edcfa4e461a091b86d42
Bunmi Oluwatudimu <bmo43@drexel.edu>
2020-09-22 12:51:55 -0400

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Bunmi Oluwatudimu on 9/22/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

dc02048d072d1c592c4a76a92a9e2ab889f7d97a
Bunmi Oluwatudimu <bmo43@drexel.edu>
2020-10-02 19:49:06 -0400

    Collection View and Navigation Tab

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Bunmi Oluwatudimu on 10/2/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
